b5e15ed7fa737b511bd11f587ed68b5bad9008b4
Stijn Klijn <stijnklijn@gmail.com>
2021-07-23 17:45:04 +0200

    New attempt

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const {
+    login,
+    register,
+    getPassword,
+    authenticate,
+    getTransactions,
+    addTransaction, 
+    changeTransaction, 
+    deleteTransaction, 
+    getHeaders, 
+    addHeader, 
+    deleteHeader} = require('./queries')
+
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.use(cors({
+    origin: 'https://stijnklijn.github.io'
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/login', login);
+app.post('/register', register);
+app.get('/getpassword/:email', getPassword);
+
+app.get('/headers/:userId', authenticate, getHeaders);
+app.post('/headers', authenticate, addHeader);
+app.delete('/headers/:id', authenticate, deleteHeader);
+
+app.get('/transactions/:userId', authenticate, getTransactions);
+app.post('/transactions', authenticate, addTransaction);
+app.put('/transactions/:id', authenticate, changeTransaction);
+app.delete('/transactions/:id', authenticate, deleteTransaction);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
---
+++ server/queries.js
@@ -0,0 +1,226 @@
+require('dotenv').config();
+const Pool = require('pg').Pool;
+const nodemailer = require('nodemailer');
+
+const database = new Pool ({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+function login(req, res) {
+    database.query(`
+    SELECT id FROM users
+    WHERE email = '${req.headers.email}' AND password = '${req.headers.password}'`, (err, data) => {
+        if (err) {
+            res.status(500);
+        }
+        if (data.rows[0] && data.rows[0].id) {
+            res.status(200).json(data.rows[0].id);
+        }
+        res.status(401).send();
+    })
+}
+
+function register(req, res) {
+    database.query(`
+    SELECT id FROM users
+    WHERE email = '${req.body.newEmail}'`, (err, data) => {
+        if (err) {
+            res.status(500);
+        }
+        if (data.rows[0] && data.rows[0].id) {
+            res.status(401).send();
+        }
+
+        database.query(`
+        INSERT INTO users
+        VALUES (DEFAULT, '${req.body.newEmail}', '${req.body.newPassword}')
+        RETURNING id`, (err, data) => {
+            if (err) {
+                res.status(500);
+              }
+            const newId = data.rows[0].id;
+
+            database.query(`
+            INSERT INTO headers
+            VALUES (DEFAULT, 'Unallocated income', 'true', ${newId}), 
+            (DEFAULT, 'Unallocated expenses', 'false', ${newId})`, (err, data) => {
+                if (err) {
+                    res.status(500);
+                }
+                res.status(201).json(newId);
+            })
+         })
+    })
+}
+
+function getPassword(req, res) {
+    database.query(`
+    SELECT id, password FROM users
+    WHERE email = '${req.params.email}'`, (err, data) => {
+        if (err) {
+            res.status(500);
+        }
+        if (data.rows[0] && data.rows[0].id) {
+            let transport = nodemailer.createTransport({
+                host: process.env.MAIL_HOST,
+                port: process.env.MAIL_PORT,
+                auth: {
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS
+                }
+            });
+            const message = {
+                from: process.env.MAIL_USER,
+                to: req.params.email,
+                subject: 'Your Xpensoft password / Uw Xpensoft wachtwoord',
+                text: `Your Xpensoft password is / Uw Xpensoft wachtwoord is: ${data.rows[0].password}`
+            }
+            transport.sendMail(message, (err) => {
+                if (err) {
+                     res.status(404).send();
+                }
+            })
+             res.status(200).json(data.rows[0].id);
+        }
+        res.status(404).send();
+    })
+}
+
+function authenticate(req, res, next) {
+    database.query(`
+    SELECT id FROM users
+    WHERE email = '${req.headers.email}' AND password = '${req.headers.password}'`, (err, data) => {
+        if (err) {
+            res.status(500);
+        }
+        if (data.rows[0] && data.rows[0].id && data.rows[0].id == req.headers.userid) {
+            next();
+        }
+        res.status(401);
+    })
+}
+
+function getHeaders(req, res) {
+    database.query(`
+    SELECT * FROM headers
+    WHERE user_id = ${req.params.userId}`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+        }
+        res.status(200).json(data.rows);
+    })
+}
+
+function addHeader(req, res) {
+    database.query(`
+    INSERT INTO headers
+    VALUES (DEFAULT, '${req.body.name}', ${req.body.isIncome}, ${req.body.userId})
+    RETURNING id`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+        }
+
+        database.query(`
+        SELECT * FROM headers
+        WHERE id = ${data.rows[0].id}`, (err, data) => {
+            if (err) {
+                res.status(500).send();
+            }
+            res.status(201).send(data.rows);
+        })
+     })
+}
+
+function deleteHeader(req, res) {
+    database.query(`
+    DELETE FROM headers
+    WHERE id = ${req.params.id}`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+        }
+        res.status(204).send();
+    })
+}
+
+function getTransactions(req, res) {
+    database.query(`
+    SELECT transactions.id, transactions.date, transactions.description, headers.id AS header_id, headers.name AS header, transactions.amount, headers.is_income
+    FROM transactions, headers
+    WHERE transactions.user_id = ${req.params.userId} AND transactions.header_id = headers.id`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+        }
+        res.status(200).json(data.rows);
+    })
+}
+
+function addTransaction(req, res) {
+    database.query(`
+    INSERT INTO transactions
+    VALUES (DEFAULT, '${req.body.date}', '${req.body.description}', ${req.body.headerId}, ${req.body.amount}, ${req.body.userId})
+    RETURNING id`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+        }
+
+        database.query(`
+        SELECT transactions.id, transactions.date, transactions.description, headers.id AS header_id, headers.name AS header, transactions.amount, headers.is_income
+        FROM transactions, headers
+        WHERE transactions.id = ${data.rows[0].id} AND transactions.header_id = headers.id`, (err, data) => {
+            if (err) {
+                res.status(500).send();
+             }
+            res.status(201).json(data.rows);
+        })
+    })
+}
+
+function changeTransaction(req, res) {
+    database.query(`
+    UPDATE transactions
+    SET description = '${req.body.description}', header_id = ${req.body.headerId}
+    WHERE id = ${req.params.id}`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+        }
+
+        database.query(`
+        SELECT transactions.id, transactions.date, transactions.description, headers.id AS header_id, headers.name AS header, transactions.amount, headers.is_income
+        FROM transactions, headers
+        WHERE transactions.id = ${req.params.id} AND transactions.header_id = headers.id`, (err, data) => {
+            if (err) {
+                res.status(500).send();
+            }
+            res.status(201).json(data.rows);
+        })
+    })
+}
+
+function deleteTransaction(req, res) {
+    database.query(`
+    DELETE FROM transactions
+    WHERE id = ${req.params.id}`, (err, data) => {
+        if (err) {
+            res.status(500).send();
+       }
+        res.status(204).send();
+    })
+}
+
+module.exports = {
+    login,
+    register,
+    getPassword,
+    authenticate,
+    getHeaders,
+    addHeader,
+    deleteHeader,
+    getTransactions,
+    addTransaction,
+    changeTransaction,
+    deleteTransaction,
+}
