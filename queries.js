aff88987c15fe988c9975515dedda363279f68d3
Stijn Klijn <stijnklijn@gmail.com>
2021-07-23 19:55:49 +0200

    das

--- queries.js
+++ queries.js
@@ -2,13 +2,7 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 const nodemailer = require('nodemailer');
 
-const database = new Pool ({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT
-});
+const database = new Pool ();
 
 function login(req, res) {
     database.query(`
